4c371d6661b296ce9243d9f4ab0c98f933147bc1
Muhammad El-Hindi <mu7eh7gmail.com>
2018-02-19 15:02:34 +0100

    customized ethereum shell scripts for different user

--- benchmark/ethereum/init-all.sh
+++ benchmark/ethereum/init-all.sh
@@ -6,7 +6,7 @@ cd `dirname ${BASH_SOURCE-$0}`
 i=0
 for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
-    ssh -oStrictHostKeyChecking=no dinhtta@$host $ETH_HOME/init.sh $1
+    ssh -oStrictHostKeyChecking=no $USER@$host $ETH_HOME/init.sh $1
     echo done node $host
   fi
   let i=$i+1
