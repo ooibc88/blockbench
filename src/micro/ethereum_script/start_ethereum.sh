1aa12a6e042c81005b8a0ba77ec2ad20b9d722cd
Dumi Loghin <dumitrel@comp.nus.edu.sg>
2018-02-28 05:49:43 +0000

    use a variable for geth executable

--- src/micro/ethereum_script/start_ethereum.sh
+++ src/micro/ethereum_script/start_ethereum.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+GETH="geth"
 ETH_DATA=`dirname ${BASH_SOURCE-$0}`/../data
 cd `dirname ${BASH_SOURCE-$0}`
 pwd 
@@ -10,5 +11,5 @@ mkdir -p $ETH_DATA/keystore
 cp ./key/* $ETH_DATA/keystore
 
 # ------------private chain(10 accounts)------------
-geth --datadir=$ETH_DATA init genesis.json 
-geth --datadir=$ETH_DATA --nodiscover --rpcapi="db,eth,net,web3,personal,web3" --rpc --rpcaddr "localhost" --rpcport "8545" --rpccorsdomain "*" --gasprice 0 --maxpeers 32 --networkid 9119 --unlock "0x12f029d57082315085bfb4d4d8345c92c5cdd881" --password <(echo -n "") --mine --minerthreads 4
+$GETH --datadir=$ETH_DATA init genesis.json
+$GETH --datadir=$ETH_DATA --nodiscover --rpcapi="db,eth,net,web3,personal,web3" --rpc --rpcaddr "localhost" --rpcport "8545" --rpccorsdomain "*" --gasprice 0 --maxpeers 32 --networkid 9119 --unlock "0x12f029d57082315085bfb4d4d8345c92c5cdd881" --password <(echo -n "") --mine --minerthreads 4
