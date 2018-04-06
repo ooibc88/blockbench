f7b099f7486e2a77660a4e5b9d85c81f046a2b8d
Anh Dinh <ug93tad@gmail.com>
2018-04-07 00:22:39 +0800

    minor fix on parity run script

--- benchmark/parity/start_ycsb_client.sh
+++ benchmark/parity/start_ycsb_client.sh
@@ -4,5 +4,5 @@ HOST=$2
 LOG=$3
 RATE=$4
 cd `dirname ${BASH_SOURCE-$0}`
-EXE="../../src/ycsb/driver"
-nohup $EXE -db parity -threads $THREADS -P ../../src/ycsb/workloads/workloada.spec -endpoint $HOST -txrate $RATE > $LOG 2>&1 &
+EXE="../../src/macro/kvstore/driver"
+nohup $EXE -db parity -threads $THREADS -P ../../src/macro/kvstore/workloads/workloada.spec -endpoint $HOST -txrate $RATE > $LOG 2>&1 &
