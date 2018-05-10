3854f7717acfcf4f83c5399511a1dbb23eeed046
Anh Dinh <ug93tad@gmail.com>
2018-05-10 14:25:24 +0800

    turn on the Hyperledger db for smallbank

--- benchmark/hyperledger/start-clients.sh
+++ benchmark/hyperledger/start-clients.sh
@@ -15,7 +15,7 @@ for host in `cat $HOSTS`; do
     if [[ $BENCHMARK == 'ycsb' ]]; then
       nohup ./driver -db hyperledger -threads $1 -P workloads/workloada.spec -endpoint $host:7050/chaincode -txrate $4 > $LOG_DIR/client_$host"_"$1 2>&1 &
     elif [[ $BENCHMARK == 'smallbank' ]]; then
-      nohup ./driver 10000000 $1 1000 /data/dinhtta/stat.log $host:7050/chaincode > $LOG_DIR/client_$host"_"$1 2>&1 &
+      nohup ./driver -db hyperledger 10000000 $1 1000 /data/dinhtta/stat.log $host:7050/chaincode > $LOG_DIR/client_$host"_"$1 2>&1 &
     fi
   fi
 done
