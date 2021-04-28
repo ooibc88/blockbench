0278aea4694b95b898f48f1a648217c4ffa20cf8
Ubuntu <ubuntu@ip-172-31-37-62.us-east-2.compute.internal>
2021-04-28 16:44:02 +0000

    add peer dynamic

--- benchmark/fabric-v2.2/addPeer.sh
+++ benchmark/fabric-v2.2/addPeer.sh
@@ -1,39 +1,68 @@
 #!/bin/bash
 
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -o organisationNumber -p peerNumber -t caPort"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "o:p:t:" opt
+do
+   case "$opt" in
+      p ) peerN="$OPTARG" ;;
+      o ) orgN="$OPTARG" ;;
+      t ) CA_PORT="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+if [ -z "$peerN" ] || [ -z "$orgN" ] || [ -z "$CA_PORT" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+PEER="peer${peerN}"
+ORG="org${orgN}"
+
+ echo
+ echo "adding peer$peerN in organisation $orgN"
+ echo
+
  export CORE_PEER_TLS_ENABLED=true
- export CORE_PEER_LOCALMSPID="Org1MSP"
- export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
- export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
- export CORE_PEER_ADDRESS=localhost:7051
+ export CORE_PEER_LOCALMSPID="Org${orgN}MSP"
+ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/peer0.${ORG}.example.com/tls/ca.crt
+ export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/${ORG}.example.com/users/Admin@${ORG}.example.com/msp
  export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
- export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/peerOrganizations/org1.example.com/
+ export FABRIC_CA_CLIENT_HOME=${PWD}/organizations/peerOrganizations/${ORG}.example.com/
  sleep 1
  echo
- echo "Register peer1"
+ echo "Register ${PEER}"
  echo
 
- fabric-ca-client register --caname ca-org1 --id.name peer1 --id.secret peer1pw --id.type peer --tls.certfiles ${PWD}/organizations/fabric-ca/org1/tls-cert.pem
+ fabric-ca-client register --caname ca-${ORG} --id.name ${PEER} --id.secret ${PEER}pw --id.type peer --tls.certfiles ${PWD}/organizations/fabric-ca/${ORG}/tls-cert.pem
  sleep 1
- mkdir -p organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com
+ mkdir -p organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com
  echo
- echo "Generate the peer1 msp"
+ echo "Generate the ${PEER} msp"
  echo
- fabric-ca-client enroll -u https://peer1:peer1pw@localhost:7054 --caname ca-org1 -M ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp --csr.hosts peer1.org1.example.com --tls.certfiles ${PWD}/organizations/fabric-ca/org1/tls-cert.pem
+ fabric-ca-client enroll -u https://${PEER}:${PEER}pw@localhost:${CA_PORT} --caname ca-${ORG} -M ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/msp --csr.hosts ${PEER}.${ORG}.example.com --tls.certfiles ${PWD}/organizations/fabric-ca/${ORG}/tls-cert.pem
  sleep 1
- cp ${PWD}/organizations/peerOrganizations/org1.example.com/msp/config.yaml ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/config.yaml
+ cp ${PWD}/organizations/peerOrganizations/${ORG}.example.com/msp/config.yaml ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/msp/config.yaml
 
  echo
- echo "Generate the peer1-tls certificates"
+ echo "Generate the ${PEER}-tls certificates"
  echo
  sleep 1
- fabric-ca-client enroll -u https://peer1:peer1pw@localhost:7054 --caname ca-org1 -M ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls --enrollment.profile tls --csr.hosts peer1.org1.example.com --csr.hosts localhost --tls.certfiles ${PWD}/organizations/fabric-ca/org1/tls-cert.pem
+ fabric-ca-client enroll -u https://${PEER}:${PEER}pw@localhost:${CA_PORT} --caname ca-${ORG} -M ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls --enrollment.profile tls --csr.hosts ${PEER}.${ORG}.example.com --csr.hosts localhost --tls.certfiles ${PWD}/organizations/fabric-ca/${ORG}/tls-cert.pem
  sleep 1
- cp ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/* ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
- cp ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/signcerts/* ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt
- cp ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/keystore/* ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key
+ cp ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls/tlscacerts/* ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls/ca.crt
+ cp ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls/signcerts/* ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls/server.crt
+ cp ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls/keystore/* ${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/${PEER}.${ORG}.example.com/tls/server.key
  sleep 3
  echo
- echo "creating container peer1"
+ echo "creating container ${PEER}"
  echo
- IMAGE_TAG=2.2 docker-compose -f docker/docker-compose-peer1org1.yaml up -d
+ IMAGE_TAG=2.2 docker-compose -f docker/docker-compose-${PEER}${ORG}.yaml up -d
--- benchmark/fabric-v2.2/deployCC.sh
+++ benchmark/fabric-v2.2/deployCC.sh
@@ -1,11 +1,51 @@
+
+ #!/bin/bash
+
+  helpFunction()
+  {
+     echo ""
+     echo "Usage: $0 -p peerPort -c CHANNEL_NAME -d chaincode"
+     exit 1
+  }
+
+  while getopts "t:c:d:p:o:" opt
+  do
+     case "$opt" in
+        t ) PORT="$OPTARG" ;;
+        c ) CHANNEL_NAME="$OPTARG" ;;
+        d ) CHAINCODE="$OPTARG" ;;
+        p ) peerN="$OPTARG" ;;
+        o ) orgN="$OPTARG" ;;
+        ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+     esac
+  done
+
+  if [ -z "$PORT" ] || [ -z "$CHANNEL_NAME" ] || [ -z "$CHAINCODE" ] || [ -z "$peerN" ] || [ -z "$orgN" ]
+  then
+    echo $PORT $CHANNEL_NAME $CHAINCODE
+     echo "Some or all of the parameters are empty";
+     helpFunction
+  fi
+
+PEER="peer${peerN}"
+ORG="org${orgN}"
+
+ export CORE_PEER_TLS_ENABLED=true
+ export CORE_PEER_LOCALMSPID="Org${orgN}MSP"
+ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/${ORG}.example.com/peers/peer0.${ORG}.example.com/tls/ca.crt
+ export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/${ORG}.example.com/users/Admin@${ORG}.example.com/msp
+ export CORE_PEER_ADDRESS=localhost:7051
+ export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+
  echo
- echo "join peer to channel"
+ echo "join peer to channel ${CHANNEL_NAME}"
  echo
- CORE_PEER_ADDRESS=localhost:8051 peer channel join -b channel-artifacts/mychannel.block
- CORE_PEER_ADDRESS=localhost:8051 peer channel list
+ CORE_PEER_ADDRESS=localhost:${PORT} peer channel join -b channel-artifacts/${CHANNEL_NAME}.block
+ CORE_PEER_ADDRESS=localhost:${PORT} peer channel list
 sleep 10
  echo "install chaincode"
- CORE_PEER_ADDRESS=localhost:8051 peer lifecycle chaincode install kvstore.tar.gz
+ CORE_PEER_ADDRESS=localhost:${PORT} peer lifecycle chaincode install ${CHAINCODE}.tar.gz
 
-#peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls true --cafile $ORDERER_CA -C mychannel -n kvstore --peerAddresses localhost:8051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"Args":["Write","name","Bob"]}'
-#peer chaincode query -C mychannel -n kvstore -c '{"Args":["Read","name"]}'
+#peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls true --cafile $ORDERER_CA -C ${CHANNEL_NAME} -n ${CHAINCODE} --peerAddresses localhost:${PORT} --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"Args":["Write","name","Bob"]}'
+#peer chaincode query -C ${CHANNEL_NAME -n ${CHAINCODE -c '{"Args":["Read","name"]}'
