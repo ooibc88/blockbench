38694d108bd99e96044eb72d08dd897d9770f089
wejdeneHaouari <haouari.wejdene1@gmail.com>
2021-05-02 03:15:43 +0100

    working on saturation exps

---
+++ src/macro/kvstore/exprience.py
@@ -0,0 +1,38 @@
+#!/bin/python
+
+import os
+import time
+import sys
+
+# running experiments
+EXPS = [(1, 20)]
+TARGET = "fabric-v2.2"
+WORKLOAD = "workloada.spec"
+ENDPOINT = "localhost:8800,localhost:8801"
+SC = "ycsb"
+WAIT_TIME = 20
+IS_INT = 1
+OUTPUT_FILE = "output.txt"
+
+
+def run_exp():
+    cmd = './driver -db {} -threads {} -P workloads/{} -txrate {} -endpoint {} -wl {} -wt {} -isint {} 2>&1 | tee {}'
+
+    # for t in THREADS:
+    #  for r in RATES:
+    for (t, r) in EXPS:
+        # run the experiments
+        print("#### ####")
+        print(cmd.format(TARGET, t, WORKLOAD, r, ENDPOINT, SC, WAIT_TIME, IS_INT, OUTPUT_FILE))
+        print("#### ####")
+        os.system(cmd.format(TARGET, t, WORKLOAD, r, ENDPOINT, SC, WAIT_TIME, IS_INT, OUTPUT_FILE))
+
+
+
+if __name__ == '__main__':
+   start = time.time()
+   run_exp()
+   interval = time.time() - start
+   print(2000/interval)
+   print(interval)
+   print("done")
