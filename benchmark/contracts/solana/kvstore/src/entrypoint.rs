be5d4914b2c55c6a771d0846955e7c687227eaa7
alexkaf <alexkafiris@gmail.com>
2022-05-19 16:45:45 +0300

    Delete useless lines of code

--- benchmark/contracts/solana/kvstore/src/entrypoint.rs
+++ benchmark/contracts/solana/kvstore/src/entrypoint.rs
@@ -13,17 +13,5 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    // let mut accounts_iter = accounts.into_iter();
-    // let data_account = next_account_info(&mut accounts_iter)?;
-    //
-    Processor::process(accounts, instruction_data)?;
-    // let a = Instruction::unpack(&instruction_data)?;
-    // let mut stored_data = data_account.try_borrow_data()?;
-    // let hashmap = HashMap::<String, String>::new();
-    // hashmap.serialize(&mut stored_data.to_vec());
-    // let other_hashmap = HashMap::<String, String>::try_from_slice(&stored_data[..]);
-    // msg!("{:?}", hashmap);
-    // msg!("{:?}", stored_data);
-    // msg!("{:?}", other_hashmap);
-    Ok(())
+    Processor::process(accounts, instruction_data)
 }
